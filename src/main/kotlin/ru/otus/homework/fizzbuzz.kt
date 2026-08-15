8ba38f8fdd6547ac2a079f9f0513e98d98d4499b
Denis Romanov <romanovdenis@edna.ru>
2026-08-15 14:39:07 +0300

    Домашнее задание к занятию «Kotlin: Основной синтаксис»

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,14 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val array = Array(n) { index ->
+        when {
+            index == 0 -> "FizzBuzz"
+            index % 15 == 0 -> "FizzBuzz"
+            index % 3 == 0 -> "Fizz"
+            index % 5 == 0 -> "Buzz"
+            else -> index.toString()
+        }
+    }
+    return array
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,12 @@
 package ru.otus.homework
 
-
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    for (index in numbers.indices) {
+        for (secondIndex in index + 1 until numbers.size) {
+           if (numbers[index] + numbers[secondIndex] == target) {
+               return intArrayOf(index, secondIndex)
+           }
+        }
+    }
+    throw IllegalArgumentException("Numbers not found")
 }
